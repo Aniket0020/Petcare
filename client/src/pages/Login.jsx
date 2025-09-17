48bc7301140970c1c0ec6aed40edff25d4eca4ca
Aniket Ahire <aniketahire008@gmail.com>
2025-09-17 11:08:12 +0530

    login & register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,10 +3,9 @@ import * as Yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
-  
 
 const Login = () => {
- const URL = import.meta.env.VITE_API_URL;
+  const URL = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -37,100 +36,110 @@ const Login = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4 bg-gradient-to-br from-blue-50 to-indigo-50">
-      <div className="bg-white/80 backdrop-blur-md border border-gray-200 shadow-2xl p-7 rounded-3xl w-full max-w-md">
-        <h2 className="text-3xl font-bold text-center  mb-5 tracking-tight">
-          Welcome Back 🐾
-        </h2>
+      <div className="w-full max-w-5xl  flex flex-col md:flex-row justify-between shadow-2xl rounded-3xl md:rounded-3xl ">
+        <div className="md:w-[60%] w-full relative ">
+          <img
+            className="w-full h-64 md:h-full object-cover  rounded-l-3xl md:rounded-l-3xl"
+            src="/img/login2.jpg"
+            alt="Pet NFC Tag"
+          />
+        </div>
 
-        <Formik
-          initialValues={{ email: "", password: "", role: "user" }}
-          onSubmit={handleLogin}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <div className="mb-6">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Email
-                </label>
-                <Field
-                  name="email"
-                  type="email"
-                  className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
-                  placeholder="you@example.com"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="p"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
+        <div className="bg-white/80 backdrop-blur-md border border-gray-200  p-7  w-full max-w-md mx-auto  rounded-r-3xl md:rounded-r-3xl">
+          <h2 className="text-3xl font-bold text-center mb-5 tracking-tight">
+            Welcome Back 🐾
+          </h2>
 
-              <div className="mb-6">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Password
-                </label>
-                <Field
-                  name="password"
-                  type="password"
-                  className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
-                  placeholder="••••••••"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="p"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-
-              <div className="mb-6">
-                <label
-                  htmlFor="role"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Login as
-                </label>
-                <Field
-                  as="select"
-                  name="role"
-                  className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
-                >
-                  <option value="doctor">Doctor</option>
-                  <option value="user">User</option>
-                </Field>
-              </div>
-
-              {error && (
-                <p className="text-red-600 text-center mb-4 p-4 ">{error}</p>
-              )}
-
-              <button
-                type="submit"
-                disabled={isSubmitting || loading}
-                className={`w-full bg-primary text-white py-3 rounded-xl hover:bg-primary/80 transition duration-300 font-semibold shadow-md  ${
-                  loading ? "bg-primary/80" : "bg-primary"
-                }`}
-              >
-                {loading ? "Logging in..." : "Login"}
-              </button>
-            </Form>
-          )}
-        </Formik>
-
-        <p className="text-center text-sm text-gray-600 mt-6">
-          Don’t have an account?{" "}
-          <Link
-            to="/Register"
-            className="text-primary hover:underline font-medium"
+          <Formik
+            initialValues={{ email: "", password: "", role: "user" }}
+            onSubmit={handleLogin}
           >
-            Register
-          </Link>
-        </p>
+            {({ isSubmitting }) => (
+              <Form>
+                <div className="mb-6">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Email
+                  </label>
+                  <Field
+                    name="email"
+                    type="email"
+                    className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
+                    placeholder="you@example.com"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="p"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Password
+                  </label>
+                  <Field
+                    name="password"
+                    type="password"
+                    className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
+                    placeholder="••••••••"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="p"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label
+                    htmlFor="role"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Login as
+                  </label>
+                  <Field
+                    as="select"
+                    name="role"
+                    className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
+                  >
+                    <option value="doctor">Doctor</option>
+                    <option value="user">User</option>
+                  </Field>
+                </div>
+
+                {error && (
+                  <p className="text-red-600 text-center mb-4 p-4 ">{error}</p>
+                )}
+
+                <button
+                  type="submit"
+                  disabled={isSubmitting || loading}
+                  className={`w-full bg-primary text-white py-3 rounded-xl hover:bg-primary/80 transition duration-300 font-semibold shadow-md  ${
+                    loading ? "bg-primary/80" : "bg-primary"
+                  }`}
+                >
+                  {loading ? "Logging in..." : "Login"}
+                </button>
+              </Form>
+            )}
+          </Formik>
+
+          <p className="text-center text-sm text-gray-600 mt-6">
+            Don’t have an account?{" "}
+            <Link
+              to="/Register"
+              className="text-primary hover:underline font-medium"
+            >
+              Register
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,7 +5,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
 
 const Register = () => {
-const URL = import.meta.env.VITE_API_URL;
+  const URL = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
@@ -49,126 +49,135 @@ const URL = import.meta.env.VITE_API_URL;
 
   return (
     <div className="min-h-screen flex items-center justify-center px-2 bg-gradient-to-br from-blue-50 to-indigo-50 ">
-      <div className="bg-white/80 backdrop-blur-md border border-gray-200 shadow-2xl p-7 rounded-3xl w-full max-w-md">
-        <h2 className="text-3xl font-bold text-center  mb-5 tracking-tight">
-          Create Account 🐶
-        </h2>
+      <div className="w-full max-w-5xl flex flex-col md:flex-row justify-between shadow-2xl rounded-3xl md:rounded-3xl">
+        <div className="md:w-[60%] w-full relative ">
+          <img
+            className="w-full h-64 md:h-full object-cover  rounded-l-3xl md:rounded-l-3xl"
+            src="/img/reg2.jpg"
+            alt="Pet NFC Tag"
+          />
+        </div>
+        <div className="bg-white/80 backdrop-blur-md border border-gray-200  p-7  w-full max-w-md mx-auto  rounded-r-3xl md:rounded-r-3xl">
+          <h2 className="text-3xl font-bold text-center  mb-5 tracking-tight">
+            Create Account 🐶
+          </h2>
 
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <div className="mb-6">
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Username
-                </label>
-                <Field
-                  name="name"
-                  type="text"
-                  className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
-                  placeholder="Your name"
-                />
-                <ErrorMessage
-                  name="name"
-                  component="div"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-
-              <div className="mb-6">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Email
-                </label>
-                <Field
-                  name="email"
-                  type="email"
-                  className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
-                  placeholder="you@example.com"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-
-              <div className="mb-6">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Password
-                </label>
-                <Field
-                  name="password"
-                  type="password"
-                  className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
-                  placeholder="••••••••"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-
-              <div className="mb-6">
-                <label
-                  htmlFor="role"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Register as
-                </label>
-                <Field
-                  as="select"
-                  name="role"
-                  className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
-                >
-                  <option value="user">User</option>
-                  <option value="doctor">Doctor</option>
-                </Field>
-                <ErrorMessage
-                  name="role"
-                  component="div"
-                  className="text-red-500 text-sm mt-1"
-                />
-              </div>
-
-              {error && (
-                <p className="text-red-500 text-center mb-4">{error}</p>
-              )}
-
-              <button
-                type="submit"
-                disabled={isSubmitting || loading}
-                className={`w-full text-white py-3 rounded-xl transition duration-300 font-semibold shadow-md ${
-                  loading ? "bg-primary/80" : "bg-primary"
-                }`}
-              >
-                {loading ? "Registering..." : "Register"}
-              </button>
-            </Form>
-          )}
-        </Formik>
-
-        <p className="text-center text-sm text-gray-600 mt-6">
-          Already have an account?{" "}
-          <Link
-            to="/login"
-            className="text-primary hover:underline font-medium"
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
           >
-            Login
-          </Link>
-        </p>
+            {({ isSubmitting }) => (
+              <Form>
+                <div className="mb-6">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Username
+                  </label>
+                  <Field
+                    name="name"
+                    type="text"
+                    className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
+                    placeholder="Your name"
+                  />
+                  <ErrorMessage
+                    name="name"
+                    component="div"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Email
+                  </label>
+                  <Field
+                    name="email"
+                    type="email"
+                    className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
+                    placeholder="you@example.com"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Password
+                  </label>
+                  <Field
+                    name="password"
+                    type="password"
+                    className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
+                    placeholder="••••••••"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label
+                    htmlFor="role"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Register as
+                  </label>
+                  <Field
+                    as="select"
+                    name="role"
+                    className="w-full px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-emerald-400 focus:outline-none transition"
+                  >
+                    <option value="user">User</option>
+                    <option value="doctor">Doctor</option>
+                  </Field>
+                  <ErrorMessage
+                    name="role"
+                    component="div"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+
+                {error && (
+                  <p className="text-red-500 text-center mb-4">{error}</p>
+                )}
+
+                <button
+                  type="submit"
+                  disabled={isSubmitting || loading}
+                  className={`w-full text-white py-3 rounded-xl transition duration-300 font-semibold shadow-md ${
+                    loading ? "bg-primary/80" : "bg-primary"
+                  }`}
+                >
+                  {loading ? "Registering..." : "Register"}
+                </button>
+              </Form>
+            )}
+          </Formik>
+
+          <p className="text-center text-sm text-gray-600 mt-6">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="text-primary hover:underline font-medium"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
